05746ec59644459ea32bfab09fec31b94a26c35c
Wino.Ltd <72581300+winoproject@users.noreply.github.com>
2021-01-14 18:35:59 +0000

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -3,11 +3,11 @@ rom_manifest_url="https://github.com/LineageOS/android"
 rom_manifest_branch="lineage-18.1"
 
 # Local manifest (dt,vt,kt,dependencies will sync from here)
-local_manifest_url="https://github.com/Apon77/android_.repo_local_manifests"
+local_manifest_url="https://github.com/winoproject/android_.repo_local_manifests/blob/master/local_manifest.xml"
 local_manifest_branch="aex"
 
 # Lunch device
-lunch="brunch lineage_mido-user"
+lunch="brunch lineage_cheeseburger-user"
 
 # Build command
 build_command="build/envsetup.sh lunch lineage_cheeseburger-userdebug make lineage -j$(nproc --all) | tee log.txt"
