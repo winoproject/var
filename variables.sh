98cdb8d23101704e59e28b88583c73f87299a620
Khalakuzzaman Apon <61594531+Apon77@users.noreply.github.com>
2021-01-14 14:10:06 +0600

    Create variables.sh

---
+++ variables.sh
@@ -0,0 +1,16 @@
+# Repo URLs
+rom_manifest_url="git://github.com/AospExtended/manifest.git"
+rom_manifest_branch="11.x"
+
+# Local manifest (dt,vt,kt,dependencies will sync from here)
+local_manifest_url="https://github.com/Apon77/android_.repo_local_manifests"
+local_manifest_branch="aex"
+
+# Lunch device
+lunch="brunch aosp_mido-user"
+
+# Build command
+build_command="make aex"
+
+# Telegram Bot
+your_telegram_id="571213272"
